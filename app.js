79d7c52dae4b53244415b579a437615c52d7f62e
Rohan Menon <rohanmenon.nmims@gmail.com>
2019-02-24 09:39:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post("/summarize/text",async(req,res)=>{
           console.log("Body:\n\n\n",await finalSummary)
           console.log("Outside: \n\n",finalSummary)
           res.send({textData: finalSummary})
-6      }
+      }
   }
 );
 })
